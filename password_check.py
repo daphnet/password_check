d41d904f8e8b2a726ae7f1bc859eeaf56af4d3ce
daphne tang <daphne.ch.tang@gmail.com>
2020-08-10 14:58:31 +0800

    final updated

--- password_check.py
+++ password_check.py
@@ -2,10 +2,14 @@ password = 'a123456'
 
 i = 3 #remaing tries
 while i > 0:
+    i = i - 1
     pwd = input ('Please enter your password: ')
     if pwd == password:
         print ('Login Successfully')
         break #exit loop
     else:
-        i = i - 1
-        print ('Wrong password, you can try', i, 'times.') 
+        print ('Wrong password.')
+        if i > 0:
+            print ('You can try', i, 'times.')
+        else:
+            print ('Your password has been locked.')
